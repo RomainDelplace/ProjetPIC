9e1ea9c17c554a8f63ceee1b6d6c823e27436115
Romain DELPLACE <romain.delplace@gmail.com>
2021-10-28 10:29:31 +0200

    Ajout de la réfraction en cours

--- test.py
+++ test.py
@@ -11,27 +11,50 @@ import matplotlib.pyplot as plt
 from scipy.signal import hilbert
 import pandas
 from scipy.fft import fft
+viteau=1500#m.s^-1
+densiteEau=1,33
 
 def TempsEmissionReflexion(d,h):
     distance= np.sqrt(d**2+h**2)
-    viteau=1500#m.s^-1
+    
     t=distance/viteau
     
     return t
 
+def TempsSignalRefraction(distance,hauteurbio,nbio,vitessebio,alpha,hauteuremeteur):
+    hypo=hauteuremeteur/np.sin(alpha)
+    beta=np.arcsin((densiteEau*np.sin(alpha))/nbio)
+    hypobio=hauteurbio/np.cos(beta)
+    tempseau=(2*hypo)/viteau
+    tempsbio=(2*hypobio)/vitessebio
+    tempstotal=tempseau+tempsbio
+    return tempstotal
+    
+    
+    
 
-time=np.arange(0,0.001,0.000000001)
+time=np.arange(0,6,0.01)
 Initial=np.zeros(np.shape(time))
 for i in range(len(time)):
     Initial[i]=np.sin(time[i])
 plt.plot(Initial)
-t=TempsEmissionReflexion(0.4, 0.4)
-Premièresinus=np.zeros(np.shape(time))
+tref=TempsEmissionReflexion(0.4, 0.4)
+reflexion=np.zeros(np.shape(time))
 j = 0
 for i in range(len(time)):
-    if i*100<=t:
-        Premièresinus[i]=0
+    if i*100<=tref:
+        reflexion[i]=0
     else:
-       Premièresinus[i]=np.sin(time[j])
+       reflexion[i]=0.5*np.sin(time[j])
        j=j+1
-plt.plot(Premièresinus)
+plt.plot(reflexion)
+trac=TempsSignalRefraction(0,4,1*10**-6,  ,  ,  ,0,4)
+refraction=np.zeros(np.shape(time))
+k = 0
+for i in range(len(time)):
+    if i*100<=trac:
+        refraction[i]=0
+    else:
+       refraction[i]=0.5*np.sin(time[k])
+       k=k+1
+plt.plot(refraction)
