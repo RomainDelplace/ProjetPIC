4b11b3441248d95b782c0e9f1e5a00dadff0dfef
Romain DELPLACE <romain.delplace@gmail.com>
2021-11-09 20:12:04 +0100

    Petite modification du calcul

--- calcul_equation.py
+++ calcul_equation.py
@@ -16,23 +16,16 @@ def resultat(Xb, Yb, Xr, Yr, Ys, c1, c2, h1, h2):
    #result = sy.solve(((Xs-Xb)*(Xs-Xb))/(c2*c2*((Xs-Xb)*(Xs-Xb)+(Ys-Yb)*(Ys-Yb)))-((Xr-Xs)*(Xr-Xs))/(c1*c1*((Xr-Xs)*(Xr-Xs)+(Yr-Ys)*(Yr-Ys))), Xs)  
    #result = sy.solveset(((Xs-Xb)*(Xs-Xb))/(c2*c2*((Xs-Xb)*(Xs-Xb)+(Ys-Yb)*(Ys-Yb)))-((Xr-Xs)*(Xr-Xs))/(c1*c1*((Xr-Xs)*(Xr-Xs)+(Yr-Ys)*(Yr-Ys))), Xs)
    result_root = sy.root(((Xs-Xb)*(Xs-Xb))/(c2*c2*((Xs-Xb)*(Xs-Xb)+(Ys-Yb)*(Ys-Yb)))-((Xr-Xs)*(Xr-Xs))/(c1*c1*((Xr-Xs)*(Xr-Xs)+(Yr-Ys)*(Yr-Ys))), Xs)  
-
-   
-   x = np.linspace(-4, 4, 1001)
-   exec('y = %s' % str(result_root).replace('Xs','x'))
-   plt.plot(x, y)
-   plt.show()
    return result
 
 Xb = 0.20
 Yb = 0.50
 Xr = 0.40
 Yr = 0
-Ys = 0.40
+Ys = 0.4
 c1 = 1.48*10**6
 c2 = 1.51*10**6
-h1 = 0.001
+h1 = 0.1
 h2 = 0
 resultat_xs = resultat(Xb, Yb, Xr, Yr, Ys, c1, c2, h1, h2)
 print(resultat_xs)
-
