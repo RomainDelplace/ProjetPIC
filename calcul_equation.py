a21a2e90d5acf061375b46810b8c01cd312cf4dd
Romain DELPLACE <romain.delplace@gmail.com>
2021-12-20 12:55:23 +0100

    update code: Ajout de toutes les reflexions

--- calcul_equation.py
+++ calcul_equation.py
@@ -20,13 +20,13 @@ def resultat_prim(Xb, Yb, Xe, Ye, Ys, c1, c2):
    result_prim = sy.solve((((Xs-Xe)**2)/(c1**2*((Xe-Xs)**2+(Ye-Ys)**2)))-(((Xb-Xs)**2)/(c2**2*((Xb-Xs)**2+(Yb-Ys)**2))), Xs) 
    return result_prim
 
-Xb = 0.20
-Yb = 0.5
+Xb = -0.20
+Yb = -0.5
 Xe = 0
 Ye = 0
-Xr = 0.40
+Xr = -0.40
 Yr = 0
-Ys = 0.4
+Ys = -0.4
 c1 = 1.48*10**3
 c2 = 1.2*c1
 
@@ -62,32 +62,6 @@ def calculEB(Xb, Yb, Xe, Ye, Ys, c1, c2):
     return resultat_possible
 
 
-
-
-
-
-
-
-
-#np.complex(resultat_xs)
-#resultat_first_xs = resultat(Xb, Yb, Xr, Yr, Ys, c1, c2, h1, h2)[0]
-#resultat_second_xs = resultat(Xb, Yb, Xr, Yr, Ys, c1, c2, h1, h2)[1]
-
-#A = sy.symbols('A')
-#B = sy.symbols('B')
-#C = sy.symbols('C')
-#D = sy.symbols('E')
-#E = sy.symbols('A')
-#x = sy.symbols('x')
-#y = sy.symbols('y')
-#z = sy.symbols('z')
-#t = sy.symbols('t')
-#from sympy.solvers.solveset import linsolve
-
-#equation = linsolve([B/A-x-y-z-t, z*t+(x+y)*(z+t)+x*y-C/A, (x+y)*z*t-(z+t)*x*y-D/A, E/A-x*y*z*t],(x,y,z,t))
-
-
-
 def resultat2(Xb, Yb, Xr, Yr, Ys, c1, c2):
    Xs = sy.symbols('Xs')
    result = sy.solve(((3*(Xs)**2)+Xs+9), Xs)  
@@ -97,3 +71,14 @@ def resultat2(Xb, Yb, Xr, Yr, Ys, c1, c2):
    #result = sy.solveset(((Xs-Xb)*(Xs-Xb))/(c2*c2*((Xs-Xb)*(Xs-Xb)+(Ys-Yb)*(Ys-Yb)))-((Xr-Xs)*(Xr-Xs))/(c1*c1*((Xr-Xs)*(Xr-Xs)+(Yr-Ys)*(Yr-Ys))), Xs)
    #result_root = sy.root(((Xs-Xb)*(Xs-Xb))/(c2*c2*((Xs-Xb)*(Xs-Xb)+(Ys-Yb)*(Ys-Yb)))-((Xr-Xs)*(Xr-Xs))/(c1*c1*((Xr-Xs)*(Xr-Xs)+(Yr-Ys)*(Yr-Ys))), Xs)  
    return result
+
+
+
+
+
+
+
+
+
+
+
